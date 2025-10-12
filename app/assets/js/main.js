dcf368b4a6646cbb6d215267f3a7f523ee6be943
AkingbadeOmosebi <akingbadeomosebi@gmail.com>
2025-10-13 00:39:31 +0200

    switched to megalinter, hence config for lint and megalinter

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function($) {
 		  	arrows: true,
 		  	autoplay: true,
 		  	autoplaySpeed: 4000,
-		  	prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='ri-arrow-left-line'></i></button>",
-		  	nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='ri-arrow-right-line'></i></button>"
+		  	prevArrow:'<button type=\'button\' class=\'slick-prev slick-arrow\'><i class=\'ri-arrow-left-line\'></i></button>',
+		  	nextArrow:'<button type=\'button\' class=\'slick-next slick-arrow\'><i class=\'ri-arrow-right-line\'></i></button>'
 		});
     }
    
